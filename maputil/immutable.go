2e4690e857148596115d41236f211404d99241a3
lizexin <546973743@qq.com>
2023-10-28 14:40:17 +0800

    feat: maputil

---
+++ maputil/immutable.go
@@ -0,0 +1,49 @@
+package maputil
+
+type ImmutableMap[K comparable, V any] struct {
+	m map[K]V
+}
+
+func NewImmutableMap[K comparable, V any](m map[K]V) *ImmutableMap[K, V] {
+	if m == nil {
+		m = make(map[K]V)
+	}
+	return &ImmutableMap[K, V]{
+		m: m,
+	}
+}
+
+func (i *ImmutableMap[K, V]) Get(k K) (V, bool) {
+	var (
+		v V
+		b bool
+	)
+	if i.m != nil {
+		v, b = i.m[k]
+	}
+	return v, b
+}
+
+func (i *ImmutableMap[K, V]) HasKey(k K) bool {
+	_, b := i.Get(k)
+	return b
+}
+
+func (i *ImmutableMap[K, V]) AllKeys() []K {
+	ks := make([]K, 0, len(i.m))
+	for k := range i.m {
+		ks = append(ks, k)
+	}
+	return ks
+}
+
+func (i *ImmutableMap[K, V]) Range(fn func(K, V) bool) {
+	if fn == nil {
+		return
+	}
+	for k, v := range i.m {
+		if !fn(k, v) {
+			return
+		}
+	}
+}
---
+++ maputil/map_test.go
@@ -0,0 +1,23 @@
+package maputil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewConcurrentMap(t *testing.T) {
+	m := NewConcurrentMap[string, string](nil)
+	fmt.Println(m.LoadOrStore("k", "v"))
+	fmt.Println(m.Load("k"))
+	fmt.Println(m.LoadOrStoreWithLoader("k", func() (string, error) {
+		return "v2", nil
+	}))
+	fmt.Println(m.Load("k"))
+	fmt.Println(m.LoadOrStoreWithLoader("k2", func() (string, error) {
+		return "v2", nil
+	}))
+	fmt.Println(m.Load("k2"))
+	fmt.Println(m.AllKeys())
+	m.RemoveKey("k")
+	fmt.Println(m.AllKeys())
+}
