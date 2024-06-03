a6ca09f306de7219aba17ea7dc894ee9f4fe5d42
lizexin <546973743@qq.com>
2024-06-03 13:13:33 +0800

    feat: task

--- taskutil/maintask.go
+++ taskutil/maintask.go
@@ -12,21 +12,43 @@ import (
 mainLoopTask 集群节点中，只有单个节点执行，其他节点会等待执行
 */
 type mainLoopTask struct {
-	handler                     func(context.Context)
-	leaser                      lease.Leaser
-	subCancelFn                 atomic.Value
-	waitDuration, renewDuration time.Duration
+	handler                      func(context.Context)
+	leaser                       lease.Leaser
+	subCancelFn                  atomic.Value
+	waitDuration, renewDuration  time.Duration
+	grantCallback, renewCallback func(bool)
+	releaseCallback              func()
 }
 
-func RunMainLoopTask(handler func(context.Context), leaser lease.Leaser, waitDuration, renewDuration time.Duration) (StopFunc, error) {
-	if handler == nil || leaser == nil || waitDuration <= 0 || renewDuration <= 0 {
+type MainLoopTaskOpts struct {
+	Handler         func(context.Context)
+	Leaser          lease.Leaser
+	WaitDuration    time.Duration
+	RenewDuration   time.Duration
+	GrantCallback   func(bool)
+	RenewCallback   func(bool)
+	ReleaseCallback func()
+}
+
+func (m *MainLoopTaskOpts) IsValid() bool {
+	if m.Handler == nil || m.Leaser == nil || m.WaitDuration <= 0 || m.RenewDuration <= 0 {
+		return false
+	}
+	return true
+}
+
+func RunMainLoopTask(opts MainLoopTaskOpts) (StopFunc, error) {
+	if !opts.IsValid() {
 		return nil, errors.New("invalid args")
 	}
 	task := &mainLoopTask{
-		handler:       handler,
-		leaser:        leaser,
-		waitDuration:  waitDuration,
-		renewDuration: renewDuration,
+		handler:         opts.Handler,
+		leaser:          opts.Leaser,
+		waitDuration:    opts.WaitDuration,
+		renewDuration:   opts.RenewDuration,
+		grantCallback:   opts.GrantCallback,
+		renewCallback:   opts.RenewCallback,
+		releaseCallback: opts.ReleaseCallback,
 	}
 	return task.Start(), nil
 }
@@ -58,7 +80,15 @@ func (t *mainLoopTask) do() {
 	// 尝试加锁
 	releaser, renewer, b, err := t.leaser.TryGrant()
 	if err == nil && b {
-		defer releaser.Release()
+		if t.grantCallback != nil {
+			t.grantCallback(true)
+		}
+		defer func() {
+			releaser.Release()
+			if t.releaseCallback != nil {
+				t.releaseCallback()
+			}
+		}()
 		t.handler(ctx)
 		// 续期
 		go func() {
@@ -69,9 +99,19 @@ func (t *mainLoopTask) do() {
 				}
 				renewRet, err := renewer.Renew(ctx)
 				if err != nil || !renewRet {
+					if t.renewCallback != nil {
+						t.renewCallback(false)
+					}
 					return
+				}
+				if t.renewCallback != nil {
+					t.renewCallback(true)
 				}
 			}
 		}()
+	} else {
+		if t.grantCallback != nil {
+			t.grantCallback(false)
+		}
 	}
 }
